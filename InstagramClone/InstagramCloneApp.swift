d1f346bace36b08ad2eb33b6b4038ca8e8daba5a
Reed Kellett <reed.kellett@gmail.com>
2021-03-10 00:05:05 -0500

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Reed Kellett on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
